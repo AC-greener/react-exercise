0238534d55bcbed09ef95e31ddfe2e5c6865e68d
tongtong <1053673440@qq.com>
2019-01-13 18:12:59 +0800

    '重新build添加homepage'

---
+++ hello-world/build/precache-manifest.d5d306e221c5c845b89c52b6f5112c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-exercise/hello-world/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "19ae678f058b104c6db5",
+    "url": "/react-exercise/hello-world/build/static/js/runtime~main.19ae678f.js"
+  },
+  {
+    "revision": "0c358ec2477d4863c1eb",
+    "url": "/react-exercise/hello-world/build/static/js/main.0c358ec2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-exercise/hello-world/build/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "0c358ec2477d4863c1eb",
+    "url": "/react-exercise/hello-world/build/static/css/main.9873f146.chunk.css"
+  },
+  {
+    "revision": "8a53a823b51a551d961f62f71c2e46cd",
+    "url": "/react-exercise/hello-world/build/index.html"
+  }
+];
--- hello-world/build/service-worker.js
+++ hello-world/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cddac3d5fa54c2a5c7c3e1f78a32c2b.js"
+  "/react-exercise/hello-world/build/precache-manifest.d5d306e221c5c845b89c52b6f5112c8b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-exercise/hello-world/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
