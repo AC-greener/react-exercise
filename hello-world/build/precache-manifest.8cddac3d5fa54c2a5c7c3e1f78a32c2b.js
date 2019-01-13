5004444d5f28d5dacbb741b5b0e39c20e77f35d3
tongtong <1053673440@qq.com>
2019-01-13 18:02:47 +0800

    '第一次build'

---
+++ hello-world/build/precache-manifest.8cddac3d5fa54c2a5c7c3e1f78a32c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f32c1559cf42cf342145",
+    "url": "/static/js/main.f32c1559.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f32c1559cf42cf342145",
+    "url": "/static/css/main.9873f146.chunk.css"
+  },
+  {
+    "revision": "b78b34932d7ad7e3668387732f238005",
+    "url": "/index.html"
+  }
+];
